2e44c50e3a1dc4cba07148d5249e8df450ddcc73
Pétrus Pradella <petrus_ant@hotmail.com>
2024-09-11 19:38:41 -0300

    Add modern ModernMohistForgeListener using the MohistEventBus::register

--- src/main/java/br/com/finalcraft/evernifecore/listeners/forge/ForgeListener.java
+++ src/main/java/br/com/finalcraft/evernifecore/listeners/forge/ForgeListener.java
@@ -2,6 +2,7 @@ package br.com.finalcraft.evernifecore.listeners.forge;
 
 import br.com.finalcraft.evernifecore.listeners.base.ECListener;
 import br.com.finalcraft.evernifecore.listeners.forge.imp.ArclightForgeListener;
+import br.com.finalcraft.evernifecore.listeners.forge.imp.ModernMohistForgeListener;
 import br.com.finalcraft.evernifecore.listeners.forge.imp.MohistForgeListener;
 import br.com.finalcraft.evernifecore.util.FCReflectionUtil;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,8 +12,13 @@ public class ForgeListener {
 
     private static IForgeListener INSTANCE; static {
         if (FCReflectionUtil.isClassLoaded("io.izzel.arclight.api.Arclight")){
+            //Present on 1.12.2 and 1.16.5 and 1.20.1
             INSTANCE = new ArclightForgeListener();
+        }else if (FCReflectionUtil.isClassLoaded("com.mohistmc.forge.MohistEventBus")){
+            //Present on 1.20.1
+            INSTANCE = new ModernMohistForgeListener();
         }else if (FCReflectionUtil.isClassLoaded("com.mohistmc.api.event.BukkitHookForgeEvent")){
+            //present on 1.12.2 and 1.16.5
             INSTANCE = new MohistForgeListener();
         }
     }
---
+++ src/main/java/br/com/finalcraft/evernifecore/listeners/forge/imp/ModernMohistForgeListener.java
@@ -0,0 +1,31 @@
+package br.com.finalcraft.evernifecore.listeners.forge.imp;
+
+import br.com.finalcraft.evernifecore.listeners.base.ECListener;
+import br.com.finalcraft.evernifecore.listeners.forge.IForgeListener;
+import br.com.finalcraft.evernifecore.reflection.MethodInvoker;
+import br.com.finalcraft.evernifecore.util.FCReflectionUtil;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.EventBus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import org.bukkit.plugin.Plugin;
+
+public class ModernMohistForgeListener implements IForgeListener {
+
+    private static MethodInvoker<Object> MohistEventBus_register = FCReflectionUtil.getMethod(
+            FCReflectionUtil.getClass("com.mohistmc.forge.MohistEventBus"),
+            "register"
+    );
+
+    @Override
+    public void registerClass(Plugin plugin, ECListener listener, IEventBus... eventBus) {
+        for (IEventBus bus : eventBus) {
+            MohistEventBus_register.invoke((EventBus) bus, listener);
+        }
+    }
+
+    @Override
+    public void registerClass(Plugin plugin, ECListener listener) {
+        MohistEventBus_register.invoke((EventBus) MinecraftForge.EVENT_BUS, listener);
+    }
+
+}
