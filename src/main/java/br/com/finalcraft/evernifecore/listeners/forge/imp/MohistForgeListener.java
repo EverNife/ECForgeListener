9cc399c6dc28c3ca7e31f170bb2fc5ad393ae30b
Pétrus Pradella <petrus_ant@hotmail.com>
2025-07-03 23:38:33 -0300

    For MohistForgeListener, allows old SubscribeEvent annotation as well

--- src/main/java/br/com/finalcraft/evernifecore/listeners/forge/imp/MohistForgeListener.java
+++ src/main/java/br/com/finalcraft/evernifecore/listeners/forge/imp/MohistForgeListener.java
@@ -4,8 +4,6 @@ import br.com.finalcraft.evernifecore.EverNifeCore;
 import br.com.finalcraft.evernifecore.listeners.base.ECListener;
 import br.com.finalcraft.evernifecore.listeners.forge.IForgeListener;
 import com.mohistmc.api.event.BukkitHookForgeEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.Plugin;
@@ -19,13 +17,28 @@ import java.util.function.Consumer;
 
 public class MohistForgeListener implements IForgeListener, ECListener {
 
+    private static Class SubscribeEventClass;
+    static {
+        try {
+            // Modern +1.16.5 Forge
+            SubscribeEventClass = Class.forName("net.minecraftforge.eventbus.api.SubscribeEvent");
+        } catch (Throwable e1) {
+            try {
+                // Old 1.12.2 Forge
+                SubscribeEventClass = Class.forName("net.minecraftforge.fml.common.eventhandler.SubscribeEvent");
+            } catch (Throwable e2) {
+                throw new RuntimeException("Failed to find @SubscribeEvent class for MohistForgeListener", e2);
+            }
+        }
+    }
+
     public MohistForgeListener() {
         ECListener.register(EverNifeCore.instance, this);
     }
 
     @Override
     public void registerListener(Plugin plugin, ECListener listener, Object... eventBus) {
-        registerListener(plugin, listener); //Mohist don't use BUS to register
+        registerListener(plugin, listener); // Mohist does not use BUS to register
     }
 
     @Override
@@ -34,9 +47,9 @@ public class MohistForgeListener implements IForgeListener, ECListener {
         for (Method declaredMethod : listener.getClass().getDeclaredMethods()) {
             declaredMethod.setAccessible(true);
 
-            if (declaredMethod.isAnnotationPresent(SubscribeEvent.class)){
+            if (declaredMethod.isAnnotationPresent(SubscribeEventClass)){
                 try {
-                    Class<? extends Event> forgeEvent = (Class<? extends Event>) declaredMethod.getParameters()[0].getType();
+                    Class forgeEvent = declaredMethod.getParameters()[0].getType();
                     this.addEventHandler(forgeEvent, event -> {
                         try {
                             declaredMethod.invoke(listener, event);
@@ -62,7 +75,7 @@ public class MohistForgeListener implements IForgeListener, ECListener {
 
     @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
     public void onBukkitHookForgeEvent(BukkitHookForgeEvent event) {
-        Event forgeEvent = event.getEvent();
+        Object forgeEvent = event.getEvent();
         List<Consumer> consumers = eventHandlers.get(forgeEvent.getClass());
         if (consumers != null){
             for (Consumer consumer : consumers){
@@ -75,7 +88,7 @@ public class MohistForgeListener implements IForgeListener, ECListener {
         }
     }
 
-    public <T extends Event> void addEventHandler(Class<T> clazz, Consumer<T> consumer){
+    public <T> void addEventHandler(Class<T> clazz, Consumer<T> consumer){
         List<Consumer> consumers = eventHandlers.computeIfAbsent(clazz, k -> new ArrayList<>());
         consumers.add(consumer);
     }
